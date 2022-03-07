7bf288e095693ecbef15ddea75146d9d2bbcf5fa
wanarisu <wanarisu@gamil.com>
2022-03-08 02:03:25 +0900

    Start up

---
+++ Hello/MyForm.cpp
@@ -0,0 +1,10 @@
+#include "MyForm.h"
+
+using namespace Hello;
+
+[STAThreadAttribute]
+int main()
+{
+	Application::Run(gcnew MyForm());
+	return 0;
+}
---
+++ Hello/MyForm.h
@@ -0,0 +1,92 @@
+#pragma once
+
+namespace Hello {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// MyForm の概要
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ここにコンストラクター コードを追加します
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+
+	protected:
+
+
+
+	private:
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::Button^ button1;
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			button1->Location = System::Drawing::Point(183, 617);
+			button1->Name = L"button1";
+			button1->Size = System::Drawing::Size(580, 200);
+			button1->TabIndex = 1;
+			button1->Text = L"挨拶";
+			button1->UseVisualStyleBackColor = true;
+			button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(13, 24);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1347, 942);
+			this->Controls->Add(button1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+			// 挨拶を表示
+			MessageBox::Show(textBox1->Text + "さん、こんにちわ。");
+	}
+	};
+}
